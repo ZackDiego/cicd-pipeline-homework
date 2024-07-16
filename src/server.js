8dfadac63d42cfd29fca178212299f2cba9cbc9d
蔣永寬 <mrzackchiang@gmail.com>
2024-07-16 14:28:02 +0800

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const DB_PWD = process.env.DB_PWD;
+
+app.get('/', (req, res) => {
+  res.send("Hello, let's try auto deploy. v1.1");
+});
+
+app.get('/db', (req, res) => {
+  res.send(`DB_PWD: ${DB_PWD}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
