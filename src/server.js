5d04868f81734d9aa9d10fcfecfe2c499e50029a
蔣永寬 <mrzackchiang@gmail.com>
2024-07-16 15:27:38 +0800

    Try PR

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const DB_PWD = process.env.DB_PWD;
 
 app.get('/', (req, res) => {
-  res.send("Hello, let's try auto deploy. v1.1");
+  res.send("Hello, let's try PR cicd pipeline!");
 });
 
 app.get('/db', (req, res) => {
